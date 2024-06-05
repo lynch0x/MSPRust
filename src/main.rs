0293f4e32b2438d9b8ef1b0db1751077c1d92d97
mattsh3n <142609965+lynch0x@users.noreply.github.com>
2024-06-05 18:44:48 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::{any::Any, io::{stdout, Read, Write}, thread::sleep, time::Duration};
+use std::{any::Any, io::{stdout, Read, Write}, iter::RepeatWith, net::{Ipv4Addr, SocketAddr, TcpStream, ToSocketAddrs}, thread::sleep, time::Duration};
 use amfserializer::Null;
 use std::collections::HashMap;
-use reqwest::{blocking::Client, header::{HeaderMap, HeaderValue}, StatusCode};
 mod amfserializer;
 mod ticket;
 mod checksum;
@@ -13,6 +12,7 @@ use crate::amfserializer::AMFHeader;
 use crate::amfserializer::AMFMessage;
 use crate::amfserializer::AMFBody;
 use crate::amfserializer::AMFSerializer;
+
 fn send_amf(method: &str,body:Vec<Box<dyn Any>>)->Box<dyn Any>{
     let message:AMFMessage = AMFMessage{
         headers: vec![
@@ -34,28 +34,29 @@ fn send_amf(method: &str,body:Vec<Box<dyn Any>>)->Box<dyn Any>{
             body:Box::new(body)
         }
     };
-    let stream: Vec<u8> = AMFSerializer::serialize_message(&message);
- //   println!("Bytes: {:?}",stream);
-    let client = Client::new();
-    let mut httpheaders = HeaderMap::new();
-    httpheaders.insert("Content-Type", HeaderValue::from_static("application/x-amf"));
-    httpheaders.insert("Referer", HeaderValue::from_static("app:/cache/t1.bin/[[DYNAMIC]]/2"));
-    httpheaders.insert("User-Agent", HeaderValue::from_static("Mozilla/5.0 (Windows; U; en) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/32.0"));
-    let result = client.post(format!("https://ws-pl.mspapis.com/Gateway.aspx?method={}",method))
-        .body(stream).headers(httpheaders).send().unwrap();
-    if result.status() == StatusCode::OK{
-        let bytes =result.bytes().unwrap();
-        let bodybytes =bytes.split(|x| *x==u8::MAX).last().unwrap();
-       // println!("Bytes: {:?}",bodybytes);
-        return amfdeserializer::AMFDeserializer::deserialize(bodybytes);
-       
-      //  Err("Request failed");
-    }
-    return Box::new(Null);
-     //Ok(amfdeserializer::AMFDeserializer::deserialize(result.bytes().unwrap().split(|x| *x==u8::MAX).last().unwrap()));
+    let amfstream: Vec<u8> = AMFSerializer::serialize_message(&message);
+let addr:SocketAddr = "ws-pl.mspapis.com:80".to_socket_addrs().unwrap().next().unwrap();
+    let mut tcpclient = TcpStream::connect_timeout(&addr,Duration::from_secs(3)).expect("There was an error while connecting to host");
+    let mut final_vec:Vec<u8> = Vec::new();
+    final_vec.write(format!("POST /Gateway.aspx?method={} HTTP/1.1\r\n",method).as_bytes()).unwrap();
+    final_vec.write("content-type: application/x-amf\r\n".as_bytes()).unwrap();
+    final_vec.write("referer: app:/cache/t1.bin/[[DYNAMIC]]/2\r\n".as_bytes()).unwrap();
+    final_vec.write("user-agent: Mozilla/5.0 (Windows; U; en) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/32.0\r\n".as_bytes()).unwrap();
+    final_vec.write(format!("content-length: {}\r\n",amfstream.len()).as_bytes()).unwrap();
+    final_vec.write("connection: close\r\n".as_bytes()).unwrap();
+    final_vec.write("host: ws-pl.mspapis.com\r\n\r\n".as_bytes()).unwrap();
+    final_vec.write(&amfstream).unwrap();
+    tcpclient.write_all(&final_vec).expect("Could not write body");
+    let mut response = Vec::new();
+
+tcpclient.read_to_end(&mut response).expect("Could not read stream!");
+
+    let bodybytes =response.split(|x| *x==u8::MAX).last().unwrap();
+    return amfdeserializer::AMFDeserializer::deserialize(bodybytes);
+
 }
 
-fn change_profile_picture(ticket: String,actor_id:i32,bytearray: Vec<u8>){
+fn change_profile_picture(ticket: String,actor_id:i32,image_type:String,bytearray: Vec<u8>){
     let result = send_amf("MovieStarPlanet.WebService.Snapshots.AMFGenericSnapshotService.CreateSnapshot", vec![
         Box::new(TicketHeader{
             ticket: TicketGenerator::generate_header(ticket),
@@ -63,33 +64,27 @@ fn change_profile_picture(ticket: String,actor_id:i32,bytearray: Vec<u8>){
         }),
         
         Box::new(actor_id ),
-        Box::new("moviestar"),
+        Box::new(image_type),
         Box::new(bytearray),
         Box::new("jpg")]);
     println!("Profile picture changed: {:?}",result.downcast::<bool>().unwrap());
 }
-
+fn get_user_input(message: &str)->String{
+    println!("{}",message);
+    let mut input_buffer:String = String::new();
+    std::io::stdin().read_line(&mut input_buffer).expect("incorrect string");
+    let mut return_buffer:String = String::new();
+    for char in input_buffer.chars(){
+        if char != 10  as char && char != 13 as char{
+        return_buffer.push(char);
+        }
+    }
+    return return_buffer;
+}
 fn main(){
     println!("SERVER: forced to be PL");
-    let mut username_buffer:String = String::new();
-    let mut password_buffer:String = String::new();
-    println!("Username: ");
-    std::io::stdin().read_line(&mut username_buffer).expect("incorrect string");
-    println!("Password: ");
-  
-    std::io::stdin().read_line(&mut password_buffer).expect("incorrect string");
-    let mut a1:String = String::new();
-    let mut a2:String = String::new();
-    for char in username_buffer.chars(){
-        if char != 10  as char && char != 13 as char{
-        a1.push(char);
-        }
-    }
-    for char in password_buffer.chars(){
-        if char != 10  as char && char != 13 as char{
-        a2.push(char);
-        }
-    }
+    let a1 = get_user_input("Username:");
+    let a2 = get_user_input("Password:");
 let result =send_amf("MovieStarPlanet.WebService.User.AMFUserServiceWeb.Login", vec![
     Box::new(a1),
     Box::new(a2),
@@ -106,14 +101,19 @@ if status == "Success"{
    let ticket=login_status.get("ticket").unwrap().downcast_ref::<String>().unwrap();
    let actor_id=login_status.get("actor").unwrap().downcast_ref::<HashMap<String,Box<dyn Any>>>().unwrap().get("ActorId").unwrap().downcast_ref::<f64>().unwrap();
     println!("Your ticket {}",ticket);
-
+    let img_type = get_user_input("Image type (e.g room/moviestar/fullSizeMoviestar):");
+    println!("Reading image.jpg....");
     let mut image_buffer:Vec<u8> = Vec::new();
     
-   std::fs::File::open("image.jpg").unwrap().read_to_end(&mut image_buffer).unwrap();
-    change_profile_picture(ticket.to_string(),*actor_id as i32,image_buffer);
+   std::fs::File::open("image.jpg").unwrap_or_else(|x|{
+    panic!("Could not open file!");
+   }).read_to_end(&mut image_buffer).unwrap_or_else(|x|{
+    panic!("Could not read file!");
+   });
+    change_profile_picture(ticket.to_string(),*actor_id as i32,img_type,image_buffer);
     
 }
-sleep(Duration::new(6,0));
+
 
 //println!("Response {}",dupa.get("Response").unwrap().downcast_ref::<String>().unwrap());
 //change_profile_picture(bytearray);   
