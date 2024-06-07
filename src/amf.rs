3a10f6aa0a3ba21edb15b69d52b88d49202b63b0
mattsh3n <142609965+lynch0x@users.noreply.github.com>
2024-06-07 21:11:46 +0200

    Cleaner code update

--- src/amf.rs
+++ src/amf.rs
@@ -1,65 +1,419 @@
+use std::{any::Any, collections::HashMap, io::{Read, Write}, net::{SocketAddr, TcpStream, ToSocketAddrs}, time::Duration};
+
 use native_tls::TlsConnector;
 
-
-use crate::checksum::ChecksumCalculator;
-use crate::amfstuff::AMFSerializer;
-use crate::amfstuff::AMFDeserializer;
-use crate::amfstuff::AMFMessage;
-use crate::amfstuff::AMFBody;
-use crate::amfstuff::AMFHeader;
-use std::{ any::Any,io::{Write,Read},net::{ SocketAddr, TcpStream, ToSocketAddrs}, time::Duration};
-pub fn send_amf(method: &str,body:Vec<Box<dyn Any>>)->Box<dyn Any>{
-    let message:AMFMessage = AMFMessage{
+use rand::Rng;
+use md5;
+use hex;
+use sha1::{Sha1,Digest};
+pub fn send_amf(method: &str, body: Vec<Box<dyn Any>>) -> Box<dyn Any> {
+    let message = AMFMessage {
         headers: vec![
-            AMFHeader{
-            name: String::from("needClassName"),
-            must_understand: false,
-            body: Box::new(false)
-            }
-        ,
-        AMFHeader{
-            name: String::from("id"),
-            must_understand: false,
-            body: Box::new(ChecksumCalculator::create_checksum(&body))
-            }],
+            AMFHeader {
+                name: String::from("needClassName"),
+                must_understand: false,
+                body: Box::new(false),
+            },
+            AMFHeader {
+                name: String::from("id"),
+                must_understand: false,
+                body: Box::new(ChecksumCalculator::create_checksum(&body)),
+            },
+        ],
         version: 0,
-        body: AMFBody{
-            target:String::from(method),
-            response:String::from("/1"),
-            body:Box::new(body)
-        }
+        body: AMFBody {
+            target: String::from(method),
+            response: String::from("/1"),
+            body: Box::new(body),
+        },
     };
-    let amfstream: Vec<u8> = AMFSerializer::serialize_message(&message);
+
+    let amfstream = AMFSerializer::serialize_message(&message);
+
     let connector = TlsConnector::new().unwrap();
-    //let addr = SocketAddr::from_str("54.154.22.229:443").unwrap();
-    let addr:SocketAddr = "ws-pl.mspapis.com:443".to_socket_addrs().unwrap().next().unwrap();
-    let stream = TcpStream::connect_timeout(&addr,Duration::from_secs(3)).expect("There was an error while connecting to host");
-    let mut stream = connector.connect("ws-pl.mspapis.com", stream).unwrap();
-    let mut final_vec:Vec<u8> = Vec::new();
-    final_vec.write(format!("POST /Gateway.aspx?method={} HTTP/1.1\r\n",method).as_bytes()).unwrap();
-    final_vec.write("content-type: application/x-amf\r\n".as_bytes()).unwrap();
-    final_vec.write("referer: app:/cache/t1.bin/[[DYNAMIC]]/2\r\n".as_bytes()).unwrap();
-    final_vec.write("user-agent: Mozilla/5.0 (Windows; U; en) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/32.0\r\n".as_bytes()).unwrap();
-    final_vec.write(format!("content-length: {}\r\n",amfstream.len()).as_bytes()).unwrap();
-    final_vec.write("connection: close\r\n".as_bytes()).unwrap();
-    final_vec.write("host: ws-pl.mspapis.com\r\n\r\n".as_bytes()).unwrap();
+    let addr: SocketAddr = "ws-pl.mspapis.com:443"
+        .to_socket_addrs()
+        .unwrap()
+        .next()
+        .unwrap();
+
+    let stream = TcpStream::connect_timeout(&addr, Duration::from_secs(3))
+        .expect("There was an error while connecting to host");
+
+    let mut stream = connector
+        .connect("ws-pl.mspapis.com", stream)
+        .unwrap();
+
+    let mut final_vec = Vec::new();
+    final_vec
+        .write(format!("POST /Gateway.aspx?method={} HTTP/1.1\r\n", method).as_bytes())
+        .unwrap();
+    final_vec
+        .write("content-type: application/x-amf\r\n".as_bytes())
+        .unwrap();
+    final_vec
+        .write("referer: app:/cache/t1.bin/[[DYNAMIC]]/2\r\n".as_bytes())
+        .unwrap();
+    final_vec
+        .write(
+            "user-agent: Mozilla/5.0 (Windows; U; en) AppleWebKit/533.19.4 (KHTML, like Gecko) AdobeAIR/32.0\r\n"
+                .as_bytes(),
+        )
+        .unwrap();
+    final_vec
+        .write(format!("content-length: {}\r\n", amfstream.len()).as_bytes())
+        .unwrap();
+    final_vec
+        .write("connection: close\r\n".as_bytes())
+        .unwrap();
+    final_vec
+        .write("host: ws-pl.mspapis.com\r\n\r\n".as_bytes())
+        .unwrap();
     final_vec.write(&amfstream).unwrap();
-    
+
     stream.write_all(&final_vec).expect("Could not write body");
+
     let mut response = Vec::new();
+    stream
+        .read_to_end(&mut response)
+        .expect("Could not read stream!");
 
-    stream.read_to_end(&mut response).expect("Could not read stream!");
-   // std::fs::File::create("lol").unwrap().write_all(&response).unwrap();
-   
-   let http_headers_end = response.windows(4).position(|window| window == b"\r\n\r\n").unwrap_or_else(||{
-    panic!("Could not read http headers end");
-   });
-   let body = &response[http_headers_end + 4..];
-   if body.len() == 0{
-    panic!("Response body is null!");
-   }
-  // std::fs::File::create("lol").unwrap().write_all(bodybytes).unwrap();
-  //  return Box::new(amfstuff::Null);
-    return AMFDeserializer::deserialize(body);
+    let http_headers_end = response
+        .windows(4)
+        .position(|window| window == b"\r\n\r\n")
+        .unwrap_or_else(|| panic!("Could not read http headers end"));
 
+    let body = &response[http_headers_end + 4..];
+    if body.is_empty() {
+        panic!("Response body is null!");
+    }
+
+    AMFDeserializer::deserialize(body)
+}
+
+pub struct  AMFHeader{
+    pub name: String,
+    pub must_understand: bool,
+    pub body: Box<dyn Any>
+}
+pub struct AMFBody{
+    pub target:String,
+    pub response:String,
+    pub body:Box<dyn Any>
+}
+pub struct AMFMessage{
+    pub headers: Vec<AMFHeader>,
+    pub body: AMFBody,
+    pub version: u16
+}
+pub struct  AMFSerializer;
+impl AMFSerializer {
+    pub fn serialize_message(message: &AMFMessage) -> Vec<u8> {
+        let mut stream = Vec::new();
+        Self::write_uint16(&mut stream, 0);
+        Self::write_uint16(&mut stream, message.headers.len() as u16);
+        for header in &message.headers {
+            Self::write_utf8(&mut stream, &header.name);
+            Self::write_boolean(&mut stream, header.must_understand);
+            Self::write_int32(&mut stream, -1);
+            Self::write_data(&mut stream, &header.body);
+        }
+        Self::write_uint16(&mut stream, 1);
+        Self::write_utf8(&mut stream, &message.body.target);
+        Self::write_utf8(&mut stream, &message.body.response);
+        Self::write_int32(&mut stream, -1);
+        Self::write_data(&mut stream, &message.body.body);
+        stream
+    }
+
+    fn write_end_markup(stream: &mut Vec<u8>) {
+        stream.extend_from_slice(&[0, 0, 9]);
+    }
+
+    fn write_data(stream: &mut Vec<u8>, value: &Box<dyn Any>) {
+        if let Some(array) = value.downcast_ref::<Vec<Box<dyn Any>>>() {
+            Self::write_array(stream, array);
+        } else if value.is::<Null>() {
+            Self::write_null(stream);
+        } else if let Some(s) = value.downcast_ref::<String>() {
+            stream.push(2);
+            Self::write_utf8(stream, s);
+        } else if let Some(s) = value.downcast_ref::<&str>() {
+            stream.push(2);
+            Self::write_utf8_str(stream, s);
+        } else if let Some(b) = value.downcast_ref::<bool>() {
+            stream.push(1);
+            Self::write_boolean(stream, *b);
+        } else if let Some(i) = value.downcast_ref::<i32>() {
+            stream.push(0);
+            Self::write_double(stream, *i as f64);
+        } else if let Some(header) = value.downcast_ref::<TicketHeader>() {
+            stream.push(3);
+            Self::write_utf8(stream, &"Ticket".to_string());
+            stream.push(2);
+            Self::write_utf8(stream, &header.ticket);
+            Self::write_utf8(stream, &"anyAttribute".to_string());
+            Self::write_null(stream);
+            Self::write_end_markup(stream);
+        } else if let Some(array) = value.downcast_ref::<Vec<u8>>() {
+            stream.push(17); // change AMF encoding to AMF3
+            stream.push(12); // byte array marker
+            let mut len = (array.len() as u32) << 1 | 1;
+            Self::write_amf3_uint32_data(stream, &mut len);
+            stream.extend_from_slice(array);
+        }
+    }
+
+    fn write_amf3_uint32_data(stream: &mut Vec<u8>, value: &mut u32) {
+        *value &= 536870911;
+        if *value < 128 {
+            stream.push(*value as u8);
+            return;
+        }
+        if *value < 16384 {
+            stream.push(((*value >> 7 & 127) | 128) as u8);
+            stream.push((*value & 127) as u8);
+            return;
+        }
+        if *value < 2097152 {
+            stream.push(((*value >> 14 & 127) | 128) as u8);
+            stream.push(((*value >> 7 & 127) | 128) as u8);
+            stream.push((*value & 127) as u8);
+            return;
+        }
+        // TODO implement larger int
+    }
+
+    fn write_array(stream: &mut Vec<u8>, value: &Vec<Box<dyn Any>>) {
+        stream.push(10);
+        Self::write_int32(stream, value.len() as i32);
+        for item in value {
+            Self::write_data(stream, item);
+        }
+    }
+
+    fn write_double(stream: &mut Vec<u8>, value: f64) {
+        Self::write_bytes(stream, &value.to_be_bytes());
+    }
+
+    fn write_null(stream: &mut Vec<u8>) {
+        stream.push(5);
+    }
+
+    fn write_int32(stream: &mut Vec<u8>, value: i32) {
+        Self::write_bytes(stream, &value.to_be_bytes());
+    }
+
+    fn write_boolean(stream: &mut Vec<u8>, boolean: bool) {
+        stream.push(boolean as u8);
+    }
+
+    fn write_uint16(stream: &mut Vec<u8>, value: u16) {
+        Self::write_bytes(stream, &value.to_be_bytes());
+    }
+
+    fn write_utf8(stream: &mut Vec<u8>, value: &String) {
+        let bytes = value.as_bytes();
+        Self::write_uint16(stream, bytes.len() as u16);
+        Self::write_bytes(stream, bytes);
+    }
+
+    fn write_utf8_str(stream: &mut Vec<u8>, value: &str) {
+        let bytes = value.as_bytes();
+        Self::write_uint16(stream, bytes.len() as u16);
+        Self::write_bytes(stream, bytes);
+    }
+
+    fn write_bytes(stream: &mut Vec<u8>, bytes: &[u8]) {
+        stream.extend_from_slice(bytes);
+    }
+}
+
+pub struct Null;
+
+pub struct AMFDeserializer;
+
+impl AMFDeserializer {
+    pub fn deserialize(bytes: &[u8]) -> Box<dyn Any> {
+        let mut pos: usize = 2;
+        let headers_length = Self::read_uint16(bytes, &mut pos);
+        for _ in 0..headers_length {
+            Self::read_string(bytes, &mut pos);
+            Self::read_boolean(bytes, &mut pos);
+            Self::read_int32(bytes, &mut pos);
+            Self::read_data(bytes, &mut pos);
+        }
+        Self::read_uint16(bytes, &mut pos);
+        Self::read_string(bytes, &mut pos);
+        Self::read_string(bytes, &mut pos);
+        Self::read_int32(bytes, &mut pos);
+        Self::read_data(bytes, &mut pos)
+    }
+
+    pub fn read_data(bytes: &[u8], pos: &mut usize) -> Box<dyn Any> {
+        let marker = bytes[*pos];
+        *pos += 1;
+        Self::read_data_from_marker(bytes, pos, marker)
+    }
+
+    pub fn read_data_from_marker(bytes: &[u8], pos: &mut usize, marker: u8) -> Box<dyn Any> {
+        match marker {
+            0 => Box::new(Self::read_double(bytes, pos)),
+            1 => Box::new(Self::read_boolean(bytes, pos)),
+            2 => Box::new(Self::read_string(bytes, pos)),
+            3 => Box::new(Self::read_aso(bytes, pos)),
+            5 => Box::new(Null),
+            8 => Box::new(Self::read_associative_array(bytes, pos)),
+            10 => Box::new(Self::read_array(bytes, pos)),
+            11 => {
+                *pos += 10;
+                Box::new(Duration::new(0, 0))
+            }
+            16 => Box::new(Self::read_object(bytes, pos)),
+            _ => panic!("unknown type at {}", *pos),
+        }
+    }
+
+    pub fn read_object(bytes: &[u8], pos: &mut usize) -> HashMap<String, Box<dyn Any>> {
+        Self::read_string(bytes, pos);
+        Self::read_aso(bytes, pos)
+    }
+
+    pub fn read_array(bytes: &[u8], pos: &mut usize) -> Vec<Box<dyn Any>> {
+        let length = Self::read_int32(bytes, pos);
+        (0..length).map(|_| Self::read_data(bytes, pos)).collect()
+    }
+
+    pub fn read_associative_array(bytes: &[u8], pos: &mut usize) -> HashMap<String, Box<dyn Any>> {
+        *pos += 4;
+        Self::read_aso(bytes, pos)
+    }
+
+    pub fn read_int32(bytes: &[u8], pos: &mut usize) -> i32 {
+        let bytes = Self::read_bytes::<4>(bytes, pos);
+        i32::from_be_bytes(bytes)
+    }
+
+    pub fn read_aso(bytes: &[u8], pos: &mut usize) -> HashMap<String, Box<dyn Any>> {
+        let mut map = HashMap::new();
+        loop {
+            let name = Self::read_string(bytes, pos);
+            let marker = bytes[*pos];
+            *pos += 1;
+            if marker == 9 {
+                break;
+            }
+            map.insert(name, Self::read_data_from_marker(bytes, pos, marker));
+        }
+        map
+    }
+
+    pub fn read_uint16(bytes: &[u8], pos: &mut usize) -> u16 {
+        let bytes = Self::read_bytes::<2>(bytes, pos);
+        u16::from_be_bytes(bytes)
+    }
+
+    pub fn read_string(bytes: &[u8], pos: &mut usize) -> String {
+        let len = Self::read_uint16(bytes, pos);
+        let bytes = Self::read_bytes_vec(bytes, pos, len as usize);
+        String::from_utf8(bytes).unwrap()
+    }
+
+    pub fn read_boolean(bytes: &[u8], pos: &mut usize) -> bool {
+        let boolean = bytes[*pos] != 0;
+        *pos += 1;
+        boolean
+    }
+
+    pub fn read_double(bytes: &[u8], pos: &mut usize) -> f64 {
+        let array = Self::read_bytes::<8>(bytes, pos);
+        f64::from_be_bytes(array)
+    }
+
+    pub fn read_bytes<const N: usize>(bytes: &[u8], pos: &mut usize) -> [u8; N] {
+        let mut buffer = [0; N];
+        buffer.copy_from_slice(&bytes[*pos..*pos + N]);
+        *pos += N;
+        buffer
+    }
+
+    pub fn read_bytes_vec(bytes: &[u8], pos: &mut usize, len: usize) -> Vec<u8> {
+        let bytes = &bytes[*pos..*pos + len];
+        *pos += len;
+        bytes.to_vec()
+    }
+}
+
+
+
+
+
+pub struct ChecksumCalculator;
+impl ChecksumCalculator {
+    pub fn create_checksum(data: &Vec<Box<dyn Any>>) -> String {
+        let checksumable = Self::from_array(data) + "2zKzokBI4^26#oiP" + &Self::get_ticket_value(data);
+        let mut hasher = Sha1::new();
+        hasher.update(checksumable);
+        hex::encode(hasher.finalize())
+    }
+
+    pub fn get_ticket_value(data: &Vec<Box<dyn Any>>) -> String {
+        for item in data {
+            if let Some(ticketheader) = item.downcast_ref::<TicketHeader>() {
+                let podzial: Vec<&str> = ticketheader.ticket.split(',').collect();
+                if let Some(koncowka) = podzial.last() {
+                    return format!("{}{}", podzial[0], &koncowka[koncowka.len() - 5..]);
+                }
+            }
+        }
+        "XSV7%!5!AX2L8@vn".to_string()
+    }
+
+    pub fn from_array(data: &Vec<Box<dyn Any>>) -> String {
+        data.iter()
+            .filter_map(|value| {
+                if value.is::<TicketHeader>() {
+                    None
+                } else if let Some(num) = value.downcast_ref::<i32>() {
+                    Some(num.to_string())
+                } else if let Some(s) = value.downcast_ref::<String>() {
+                    Some(s.clone())
+                } else if let Some(s) = value.downcast_ref::<&str>() {
+                    Some(s.to_string())
+                } else if let Some(b) = value.downcast_ref::<bool>() {
+                    Some(if *b { "True".to_string() } else { "False".to_string() })
+                } else if let Some(array) = value.downcast_ref::<Vec<u8>>() {
+                    Some(Self::from_byte_array(array))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    pub fn from_byte_array(data: &Vec<u8>) -> String {
+        if data.len() <= 20 {
+            hex::encode(data)
+        } else {
+            let ar: Vec<u8> = (0..20).map(|i| data[data.len() / 20 * i]).collect();
+            hex::encode(ar)
+        }
+    }
+}
+pub struct TicketHeader {
+    pub ticket: String,
+}
+
+pub struct TicketGenerator;
+
+impl TicketGenerator {
+    pub fn generate_header(ticket: String) -> String {
+        let random_number: u16 = rand::thread_rng().gen_range(0..1000);
+        let random_number_str = random_number.to_string();
+        let random_number_bytes = random_number_str.as_bytes();
+        let md5_hash = format!("{:x}", md5::compute(random_number_bytes));
+        let hex_encoded = hex::encode(random_number_bytes);
+
+        format!("{}{}{}", ticket, md5_hash, hex_encoded)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,80 @@
-use std::{any::Any,collections::HashMap, io::Read};
+use std::{any::Any, collections::HashMap, fs::File, io::Read};
 mod amf;
-mod amfstuff;
-mod ticket;
-mod checksum;
-use ticket::*;
-use crate::amfstuff::Null;
-fn change_profile_picture(ticket: String,actor_id:i32,image_type:String,bytearray: Vec<u8>){
-    let result =   amf::send_amf("MovieStarPlanet.WebService.Snapshots.AMFGenericSnapshotService.CreateSnapshot", vec![
-        Box::new(TicketHeader{
-            ticket: TicketGenerator::generate_header(ticket)
-        }),
-        
-        Box::new(actor_id ),
-        Box::new(image_type),
-        Box::new(bytearray),
-        Box::new("jpg")]);
-    println!("Profile picture changed: {:?}",result.downcast::<bool>().unwrap());
+use crate::amf::{send_amf,TicketGenerator,TicketHeader,Null};
+
+fn change_profile_picture(ticket: String, actor_id: i32, image_type: String, bytearray: Vec<u8>) {
+    let result = send_amf(
+        "MovieStarPlanet.WebService.Snapshots.AMFGenericSnapshotService.CreateSnapshot",
+        vec![
+            Box::new(TicketHeader {
+                ticket: TicketGenerator::generate_header(ticket),
+            }),
+            Box::new(actor_id),
+            Box::new(image_type),
+            Box::new(bytearray),
+            Box::new("jpg"),
+        ],
+    );
+    println!("Profile picture changed: {:?}", result.downcast::<bool>().unwrap());
 }
-fn get_user_input(message: &str)->String{
-    println!("{}",message);
-    let mut input_buffer:String = String::new();
+
+fn get_user_input(message: &str) -> String {
+    println!("{}", message);
+    let mut input_buffer = String::new();
     std::io::stdin().read_line(&mut input_buffer).expect("incorrect string");
-    let mut return_buffer:String = String::new();
-    for char in input_buffer.chars(){
-        if char != 10  as char && char != 13 as char{
-        return_buffer.push(char);
-        }
-    }
-    return return_buffer;
+    input_buffer.trim().to_string()
 }
-fn main(){
+
+fn main() {
     println!("SERVER: forced to be PL");
-    let a1 = get_user_input("Username:");
-    let a2 = get_user_input("Password:");
-let result =amf::send_amf("MovieStarPlanet.WebService.User.AMFUserServiceWeb.Login", vec![
-    Box::new(a1),
-    Box::new(a2),
-    Box::new(Null),
-    Box::new(Null),
-    Box::new(Null),
-    Box::new("MSP1-Standalone:XXXXXX")
-  ]);
- let dupa = result.downcast_ref::<HashMap<String,Box<dyn Any>>>().unwrap();
-  let login_status = dupa.get("loginStatus").unwrap().downcast_ref::<HashMap<String,Box<dyn Any>>>().unwrap();
-  let status =login_status.get("status").unwrap().downcast_ref::<String>().unwrap();
-println!("Status: {}",status);
-if status == "Success"{
-   let ticket=login_status.get("ticket").unwrap().downcast_ref::<String>().unwrap();
-   let actor_id=login_status.get("actor").unwrap().downcast_ref::<HashMap<String,Box<dyn Any>>>().unwrap().get("ActorId").unwrap().downcast_ref::<f64>().unwrap();
-    println!("Your ticket {}",ticket);
-    let img_type = get_user_input("Image type (e.g room/moviestar/fullSizeMoviestar):");
-    println!("Reading image.jpg....");
-    let mut image_buffer:Vec<u8> = Vec::new();
-    
-   std::fs::File::open("image.jpg").unwrap_or_else(|x|{
-    panic!("Could not open file!, full error {}",x);
-   }).read_to_end(&mut image_buffer).unwrap_or_else(|x|{
-    panic!("Could not read file! full error: {}",x);
-   });
-    change_profile_picture(ticket.to_string(),*actor_id as i32,img_type,image_buffer);
-    
-}
+    let username = get_user_input("Username:");
+    let password = get_user_input("Password:");
 
+    let result = send_amf(
+        "MovieStarPlanet.WebService.User.AMFUserServiceWeb.Login",
+        vec![
+            Box::new(username),
+            Box::new(password),
+            Box::new(Null),
+            Box::new(Null),
+            Box::new(Null),
+            Box::new("MSP1-Standalone:XXXXXX"),
+        ],
+    );
 
-//println!("Response {}",dupa.get("Response").unwrap().downcast_ref::<String>().unwrap());
-//change_profile_picture(bytearray);   
-   
-    
-    
-    
+    let login_status = result.downcast_ref::<HashMap<String, Box<dyn Any>>>()
+        .and_then(|res| res.get("loginStatus"))
+        .and_then(|login_status| login_status.downcast_ref::<HashMap<String, Box<dyn Any>>>())
+        .unwrap();
 
-    
-       
+    let status = login_status.get("status")
+        .and_then(|status| status.downcast_ref::<String>())
+        .unwrap();
+
+    println!("Status: {}", status);
+
+    if status == "Success" {
+        let ticket = login_status.get("ticket")
+            .and_then(|ticket| ticket.downcast_ref::<String>())
+            .unwrap_or_else(|| panic!("Ticket not found"));
+
+        let actor_id = login_status.get("actor")
+            .and_then(|actor| actor.downcast_ref::<HashMap<String, Box<dyn Any>>>())
+            .and_then(|actor| actor.get("ActorId"))
+            .and_then(|actor_id| actor_id.downcast_ref::<f64>())
+            .map(|actor_id| *actor_id as i32)
+            .unwrap_or_else(|| panic!("ActorId not found"));
+
+        println!("Your ticket {}", ticket);
+        let img_type = get_user_input("Image type (e.g room/moviestar/fullSizeMoviestar):");
+        println!("Reading image.jpg....");
+
+        let mut image_buffer = Vec::new();
+        File::open("image.jpg")
+            .unwrap_or_else(|err| panic!("Could not open file! Error: {}", err))
+            .read_to_end(&mut image_buffer)
+            .unwrap_or_else(|err| panic!("Could not read file! Error: {}", err));
+
+        change_profile_picture(ticket.to_string(), actor_id, img_type, image_buffer);
+    }
 }
