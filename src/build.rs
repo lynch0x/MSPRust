25f84bd55c80312c7596c51f4a4f6ee4fedf4db4
mattsh3n <142609965+lynch0x@users.noreply.github.com>
2025-03-07 22:50:07 +0100

    Delete src/build.rs

--- src/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("cargo:rustc-link-search=native=lib"); // Path where .lib is located
-    println!("cargo:rustc-link-lib=static=MSPClient");  // Link the .lib file (without extension)
-}
